26222d3677d2971eb444f23a04b6c294e6a6302c
Marat <goodzilla365@mail.ru>
2019-06-17 22:56:39 +0300

    Личный проект. Задание - 4-1

--- js/upload.js
+++ js/upload.js
@@ -4,12 +4,14 @@ var ESC_KEYCODE = 27;
 var PHOTO_SIZE_MAX = 100;
 var PHOTO_SIZE_MIN = 0;
 var PHOTO_SIZE_CHANGE_STEP = 25;
+var PHOTO_EFFECT_VOLUME_DEFAULT = 100;
 
 var uploadFile = document.querySelector('#upload-file');
 var photoEditForm = document.querySelector('.img-upload__overlay');
 var photoEditFormClose = photoEditForm.querySelector('#upload-cancel');
 
-var photoPreviewImage = document.querySelector('.img-upload__preview').getElementsByTagName('img')[0];
+var photoPreview = document.querySelector('.img-upload__preview');
+var photoPreviewImage = photoPreview.getElementsByTagName('img')[0];
 var photoChangeSize = document.querySelector('.img-upload__scale');
 var photoSizeBigger = photoChangeSize.querySelector('.scale__control--bigger');
 var photoSizeSmaller = photoChangeSize.querySelector('.scale__control--smaller');
@@ -17,8 +19,18 @@ var photoSizeValue = photoChangeSize.querySelector('.scale__control--value');
 var photosize;
 
 var imageUploadEffects = document.querySelector('.effects__list');
-// var imageUploadEffectsLevel = document.querySelector('.img-upload__effect-level');
-// var imageEffectPin = document.querySelector('.effect-level__pin');
+var imageUploadEffectsLevel = document.querySelector('.img-upload__effect-level');
+var imageEffectLevelValue = imageUploadEffectsLevel.querySelector('.effect-level__value');
+var imageEffectPin = imageUploadEffectsLevel.querySelector('.effect-level__pin');
+var imageEffectDepth = imageUploadEffectsLevel.querySelector('.effect-level__depth');
+var effects = {
+  chrome: ['grayscale', 0, 1, ''],
+  sepia: ['sepia', 0, 1, ''],
+  marvin: ['invert', 0, 100, '%'],
+  phobos: ['blur', 0, 3, 'px'],
+  heat: ['brightness', 1, 3, '']
+};
+var value = '';
 
 var showElement = function (element) {
   if (element.classList.contains('hidden')) {
@@ -26,10 +38,17 @@ var showElement = function (element) {
   }
 };
 
+var hideElement = function (element) {
+  if (!element.classList.contains('hidden')) {
+    element.classList.add('hidden');
+  }
+};
+
 var showPhotoEditForm = function (element) {
   photosize = PHOTO_SIZE_MAX;
   showElement(element);
   document.addEventListener('keydown', PhotoEditFormEscPress);
+  applyPicturefilter(imageUploadEffects.children[0].children[0]);
 };
 
 var hidePhotoEditForm = function (element) {
@@ -56,6 +75,35 @@ var changeSizePhotoPreview = function (change) {
   photoPreviewImage.style = 'transform: scale(' + (photosize / 100) + ')';
 };
 
+var applyPicturefilter = function (element) {
+  value = element.value;
+
+  photoPreviewImage.classList = '';
+  photoPreviewImage.classList.add('effects__preview--' + value);
+
+  if (value === 'none') {
+    hideElement(imageUploadEffectsLevel);
+    photoPreview.style = '';
+  } else {
+    showElement(imageUploadEffectsLevel);
+    addEffectLevelValue(PHOTO_EFFECT_VOLUME_DEFAULT, effects[value]);
+  }
+};
+
+var addEffectLevelValue = function (percent, effect) {
+  imageEffectPin.style = 'left:' + percent + '%';
+  imageEffectDepth.style = 'width:' + percent + '%';
+  var valuePercent = (effect[2] - effect[1]) / PHOTO_EFFECT_VOLUME_DEFAULT * percent;
+  var valueInput = effect[1] + valuePercent;
+  imageEffectLevelValue.textContent = valueInput;
+  photoPreview.style = 'filter: ' + effect[0] + '(' + valueInput + effect[3] + ')';
+};
+
+var randomEffectValue = function () {
+  var randomPercent = Math.round(Math.random() * 100);
+  addEffectLevelValue(randomPercent, effects[value]);
+};
+
 uploadFile.addEventListener('change', function () {
   showPhotoEditForm(photoEditForm);
 });
@@ -73,15 +121,11 @@ photoSizeSmaller.addEventListener('click', function () {
   changeSizePhotoPreview('smaller');
 });
 
-imageUploadEffects.addEventListener('click', function () {
-  // console.log(evt);
-
-  /* while (target !== imageUploadEffects) {
-    if (target.tagName === 'input') {
-
-      applyEffect(target);
-      return;
-    }
-    target = target.parentNode;
-  } */
+imageUploadEffects.addEventListener('change', function (evt) {
+  if (evt.target.tagName === 'INPUT') {
+    applyPicturefilter(evt.target);
+  }
 });
+
+imageEffectPin.addEventListener('mouseup', randomEffectValue);
+
