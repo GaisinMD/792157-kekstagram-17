92a9b75bf1e7963cb1477e604dbdb455052e2949
Marat <goodzilla365@mail.ru>
2019-06-25 22:50:00 +0300

    Личный проект. Задание 6-1. Исправления

--- js/backend.js
+++ js/backend.js
@@ -22,23 +22,7 @@ window.backend = (function () {
 
       xhr.open('GET', url);
       xhr.send();
-    },
-
-    /* save: function (url, data, onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-
-      xhr.addEventListener('load', function () {
-        if (xhr.status === CODE_SUCCES) {
-          onLoad(xhr.response);
-        } else {
-          onError(xhr.status);
-        }
-      });
-
-      xhr.open('POST', url);
-      xhr.send(data);
-    } */
+    }
 
   };
 
