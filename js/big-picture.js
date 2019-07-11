3b1af8205d8d0c29db839d6473eec41835a8523b
Marat <goodzilla365@mail.ru>
2019-07-11 11:09:21 +0300

    Личный проект. Задание 8-1. Исправления

--- js/big-picture.js
+++ js/big-picture.js
@@ -4,15 +4,15 @@
 'use strict';
 
 (function () {
-  var BIG_PICTURE = document.querySelector('.big-picture');
-  var Picture = {
-    img: BIG_PICTURE.querySelector('.big-picture__img').children[0],
-    likes: BIG_PICTURE.querySelector('.likes-count'),
-    description: BIG_PICTURE.querySelector('.social__caption'),
-    commentsCount: BIG_PICTURE.querySelector('.comments-count'),
-    socialCommentList: BIG_PICTURE.querySelector('.social__comments'),
-    commentInput: BIG_PICTURE.querySelector('.social__footer-text'),
-    closeButton: BIG_PICTURE.querySelector('.big-picture__cancel')
+  var bigPicture = document.querySelector('.big-picture');
+  var picture = {
+    img: bigPicture.querySelector('.big-picture__img').children[0],
+    likes: bigPicture.querySelector('.likes-count'),
+    description: bigPicture.querySelector('.social__caption'),
+    commentsCount: bigPicture.querySelector('.comments-count'),
+    socialCommentList: bigPicture.querySelector('.social__comments'),
+    commentInput: bigPicture.querySelector('.social__footer-text'),
+    closeButton: bigPicture.querySelector('.big-picture__cancel')
   };
 
   var PictureItem = function (url, likes, comments, description) {
@@ -32,20 +32,20 @@
     var commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
     var fragment = document.createDocumentFragment();
 
-    window.utils.removeChildren(Picture.socialCommentList, Picture.socialCommentList.getElementsByTagName('li'));
+    window.utils.removeChildren(picture.socialCommentList, picture.socialCommentList.getElementsByTagName('li'));
 
     list.forEach(function (elem) {
       fragment.appendChild(generateComment(commentTemplate.cloneNode(true), elem));
     });
 
-    Picture.socialCommentList.appendChild(fragment);
+    picture.socialCommentList.appendChild(fragment);
   };
 
   var generateBigPicture = function (source) {
-    Picture.img.src = source.url;
-    Picture.likes.textContent = source.likes;
-    Picture.description.textContent = source.description;
-    Picture.commentsCount.textContent = source.comments.length;
+    picture.img.src = source.url;
+    picture.likes.textContent = source.likes;
+    picture.description.textContent = source.description;
+    picture.commentsCount.textContent = source.comments.length;
     generateCommentList(source.comments);
   };
 
@@ -57,8 +57,8 @@
   };
 
   var onBigPictureEscPress = function (evt) {
-    if (evt.keyCode === window.formConstVar.ESC_KEYCODE && evt.target !== Picture.commentInput) {
-      hideBigPicture(BIG_PICTURE);
+    if (evt.keyCode === window.formConstVar.ESC_KEYCODE && evt.target !== picture.commentInput) {
+      hideBigPicture(bigPicture);
     }
   };
 
@@ -68,7 +68,7 @@
 
     while (target !== window.formConstVar.pictureList) {
       if (target.classList.contains('picture')) {
-        window.utils.showElement(BIG_PICTURE);
+        window.utils.showElement(bigPicture);
         document.addEventListener('keydown', onBigPictureEscPress);
         hitElement = window.formConstVar.PHOTOS_LIST.slice().filter(function (i) {
           return (target.querySelector('.picture__img').src.search(i.url) >= 0);
@@ -85,9 +85,9 @@
 
   window.formConstVar.pictureList.addEventListener('click', showBigPicture);
 
-  Picture.closeButton.addEventListener('click', function (evt) {
+  picture.closeButton.addEventListener('click', function (evt) {
     evt.preventDefault();
-    hideBigPicture(BIG_PICTURE);
+    hideBigPicture(bigPicture);
   });
 
 })();
