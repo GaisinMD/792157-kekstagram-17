7e8962da3d0e84db979cf14390e0805923eeb9ac
Marat <goodzilla365@mail.ru>
2019-07-14 18:56:33 +0300

    Личный проект. Задание 8-3. Исправления-2

--- js/custom-validate.js
+++ js/custom-validate.js
@@ -1,4 +1,4 @@
-// Выполняемые задачи: Валидация
+// Выполняемые задачи: Валидация хэштега
 // Зависимости: constants.js, utils.js
 
 'use strict';
@@ -19,93 +19,29 @@ window.customValidation = (function () {
     HASHTAGS: document.querySelector('.text__hashtags'),
 
     validateHashtags: function (string) {
-      var hashtags = string.value;
-      var array = [];
+      var hashtags = string.value.trim();
       var valid = true;
       var inputCustomValidation;
       var customValidityMessage;
 
-      function CustomValidation() {}
-
-      CustomValidation.prototype = {
-        invalidities: [],
-
-        checkValidity: function (input) {
-          if (this.checkUnique(input)) {
-            this.addInvalidity('Не должно быть одинаковых хэштегов');
-          }
-          if (this.lonelyHashtag(input)) {
-            this.addInvalidity('Не должно быть хэштегов состоящих из одной #');
-          }
-          if (this.spaceHashtag(input)) {
-            this.addInvalidity('Хэштег должен начинатся с #');
-          }
-          if (this.middleHashtag(input)) {
-            this.addInvalidity('Хэштеги должны быть разделены пробелами');
-          }
-          if (this.tooLongHashtag(input)) {
-            this.addInvalidity('Максимальная длина хэштега - 20 символов включая решетку');
-          }
-          if (input.length > MAX_HASHTAGS_QUANTITY) {
-            this.addInvalidity('Максимальное количество хэштегов - 5');
-          }
-        },
-
-        addInvalidity: function (message) {
-          this.invalidities.push(message);
-        },
-
-        getInvalidities: function () {
-          return this.invalidities.join('. \n');
-        },
-
-        checkUnique: function (elements) {
-          if (elements) {
-            var sorted = elements.slice().sort();
-            for (var i = 1; i < sorted.length; i++) {
-              if (sorted[i - 1] === sorted[i]) {
-                return true;
-              }
-            }
-          }
-          return false;
-        },
-
-        lonelyHashtag: function (elements) {
-          return elements.some(function (element) {
-            return (element.charAt(0) === HASHTAG_BEGINNER) && (element.length === 1);
-          });
-        },
-
-        spaceHashtag: function (elements) {
-          return elements.some(function (element) {
-            return element.charAt(0) !== HASHTAG_BEGINNER;
-          });
-        },
-
-        middleHashtag: function (elements) {
-          return elements.some(function (element) {
-            return element.lastIndexOf(HASHTAG_BEGINNER) > 0;
-          });
-        },
-
-        tooLongHashtag: function (elements) {
-          return elements.some(function (element) {
-            return element.length > HASHTAG_LENGTH;
-          });
-        }
-
-      };
-
       window.customValidation.HASHTAGS.removeEventListener('input', removeSetCustomValidity);
 
-      hashtags = hashtags.trim();
-
       if (hashtags.length) {
-        array = hashtags.toLowerCase().split(SEPARATOR);
+        var array = hashtags.toLowerCase().split(SEPARATOR);
 
-        inputCustomValidation = new CustomValidation();
-        inputCustomValidation.checkValidity(array);
+        console.log(window.validateObject.Validation);
+
+        inputCustomValidation = new window.validateObject.Validation(array);
+        //
+        // inputCustomValidation.checkValidity(array);
+        //
+        inputCustomValidation.getReapitingHashtags();
+        inputCustomValidation.getLonelyHashtag(HASHTAG_BEGINNER);
+        inputCustomValidation.getSpaceHashtag(HASHTAG_BEGINNER);
+        inputCustomValidation.getMiddleHashtag(HASHTAG_BEGINNER);
+        inputCustomValidation.getTooLongHashtag(HASHTAG_LENGTH);
+        inputCustomValidation.getTooManyHashtag(MAX_HASHTAGS_QUANTITY);
+        //
         customValidityMessage = inputCustomValidation.getInvalidities();
 
         if (inputCustomValidation.invalidities.length) {
